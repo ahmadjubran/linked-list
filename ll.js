983e01e672e0d75449c812047f9785ae34ac8280
ahmadjubran <ahmadjubran.98@hotmail.com>
2022-09-15 16:49:16 +0300

    Append to the end if the Linked List

--- ll.js
+++ ll.js
@@ -16,6 +16,19 @@ class LinkedList {
       this.head = node;
     }
   }
+
+  append(value) {
+    let node = new Node(value);
+    if (!this.head) {
+      this.head = node;
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = node;
+    }
+  }
 }
 
 module.exports = LinkedList;
